57c4258047564b36d8c258aef362aa6973a7495a
GarrickWu <592069546@qq.com>
2019-01-25 18:27:52 +0800

    My First Commit

---
+++ app/src/main/java/com/example/administrator/recycler/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.administrator.recycler
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.support.v7.widget.LinearLayoutManager
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() , View.OnClickListener{
+    var mdata = ArrayList<Person>(5)
+    val adapter by lazy { Adapter(this@MainActivity,mdata) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initData()
+        sortData()
+        recycler.adapter = adapter
+        adapter.notifyDataSetChanged()
+        recycler.layoutManager = LinearLayoutManager(this)
+        add.setOnClickListener(this)
+        add.setTextColor(ContextCompat.getColor(this,R.color.colorAccent))
+        del.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View){
+        when(view.id){
+            R.id.add -> addItem()
+            R.id.del -> delItem()
+        }
+    }
+
+    fun addItem(){
+        adapter.addItem()
+    }
+
+    fun delItem(){
+        adapter.delItem()
+    }
+
+    fun initData(){
+        mdata.add(Person("sss",Random().nextInt(30),Random().nextInt(2)))
+        mdata.add(Person("aaa",Random().nextInt(30),Random().nextInt(2)))
+        mdata.add(Person("ddd",Random().nextInt(30),Random().nextInt(2)))
+        mdata.add(Person("www",Random().nextInt(30),Random().nextInt(2)))
+        mdata.add(Person("qqq",Random().nextInt(30),Random().nextInt(2)))
+        mdata.add(Person("dsa",Random().nextInt(30),Random().nextInt(2)))
+        mdata.add(Person("asd",Random().nextInt(30),Random().nextInt(2)))
+        mdata.add(Person("sad",Random().nextInt(30),Random().nextInt(2)))
+        mdata.add(Person("das",Random().nextInt(30),Random().nextInt(2)))
+        mdata.add(Person("sda",Random().nextInt(30),Random().nextInt(2)))
+    }
+
+    fun sortData(){
+        var j = 0
+        for(i in 0 until mdata.size ){
+            if(mdata[i].type == 1){
+                mdata.add(j,mdata.removeAt(i))
+                j++
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/administrator/recycler/Person.kt
@@ -0,0 +1,3 @@
+package com.example.administrator.recycler
+
+class Person(val name: String, val age: Int, val type: Int)
